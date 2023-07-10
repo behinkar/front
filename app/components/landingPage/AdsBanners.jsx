f05ad8922fd3155a9fa79793deaefc634366e210
miillaadddddd <milad7212@gmail.com>
2023-07-10 09:00:51 +0330

    Finish AdsBanners

--- app/components/landingPage/AdsBanners.jsx
+++ app/components/landingPage/AdsBanners.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function AdsBanners() {
   return (
     <div className="flex justify-center gap-4 max-w-5xl mx-auto">
-      <Banner />
-      <Banner />
+      <Banner  btnTitle='ساخت رزومه' texts={['رزومه نداری؟!','خودتو خوب معرفی کن ...']}/>
+      <Banner  btnTitle='شروع آموزش'  texts={['حرفه ای وارد شو!!']}/>
     </div>
   );
 }
@@ -13,14 +13,17 @@ export default AdsBanners;
 
 // inner componnet
 
-function Banner() {
+function Banner({btnTitle,texts}) {
   return (
     <>
-      <div class="rounded-lg p-4 shadow-md grow bg-gray-50">
-        hi dont have resume?//
-        <div className="flex justify-end">
-          <button className="inline-block mt-6 bg-gray-600 hover:bg-blue-600 text-white font-bold py-2 px-8 rounded focus:outline-none focus:shadow-outline transition duration-300">
-            ورود
+      <div class="rounded-lg flex flex-col justify-between p-4 shadow-md grow text-gray-700 bg-gray-50">
+        {texts.map((text,index)=>(
+          <p className="mt-4 text-xl " key={index}>{text}</p>
+        ))}
+       
+        <div className="flex justify-end ">
+          <button className="inline-block mt-6 bg-gray-600 text-sm hover:bg-blue-600 text-white font-bold py-2 px-8 rounded focus:outline-none focus:shadow-outline transition duration-300">
+            {btnTitle}
           </button>
         </div>
       </div>
