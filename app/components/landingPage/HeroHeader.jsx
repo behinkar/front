3cdf1d388db307b9212153d16caf553e0148ebe0
miillaadddddd <milad7212@gmail.com>
2023-07-10 11:45:43 +0330

    Add HeroHeader Component

--- app/components/landingPage/HeroHeader.jsx
+++ app/components/landingPage/HeroHeader.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function HeroHeader() {
   return (
-    <div>HeroHeader</div>
+    <div className="my-8 flex  items-center text-gray-800 gap-4 flex-col">
+      <p className="text-2xl font-bold">بیش از 500 آگهی استخدام</p>
+      <p className="text-xl animate-pulse">بهین کار سامانه کاریابی آنلاین</p>
+    </div>
   )
 }
 
