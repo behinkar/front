b418b2fdf1c1b6f18fb204dfeb9d3411e8b8767e
miillaadddddd <milad7212@gmail.com>
2023-07-24 13:58:47 +0330

    update

--- app/components/landingPage/SearchPart.jsx
+++ app/components/landingPage/SearchPart.jsx
@@ -1,45 +1,56 @@
 "use client";
 import React, { useState } from "react";
 import Select from "react-select";
-import { BiSearch } from "react-icons/bi";
+import { RiSearch2Line } from "react-icons/ri";
 
 function SearchPart() {
   return (
-    <div className="flex justify-center items-center gap-4 my-6 bg-[#b2c4e2]/[.46] rounded-b-3xl min-h-[500px]">
-      <input type="text" className="self-stretch rounded border placeholder:text-[#828080] placeholder:text-sm w-52 border-[#c9cacc] py-2 px-4 focus:outline-none focus:border-2 focus:border-blue-500 focus:shadow-outline-blue " placeholder="عنوان شغلی ..." />
-      <Select
-        className="w-52 "
-        classNamePrefix="select"
-        isClearable={true}
-        isRtl={true}
-        isSearchable={true}
-        name="color"
-        placeholder="استان..."
-        options={[
-          { value: "kerman", label: "کرمان" },
-          { value: "rafsenjan", label: "رفسنجان" },
-          { value: "vanilla", label: "مس سرچشمه" },
-        ]}
-      />
-       <Select
-        className="w-52 "
-        classNamePrefix="select"
-        isClearable={true}
-        isRtl={true}
-        isSearchable={true}
-        name="color"
-        placeholder="دسته بندی..."
-        options={[
-          { value: "kerman", label: "مهندسی" },
-          { value: "rafsenjan", label: "راننده" },
-          { value: "vanilla", label: "کارگری " },
-        ]}
-      />
-      <BiSearch
-       size={30}
-       className=' cursor-pointer hover:scale-105 text-gray-600 '
-       color=""
-      />
+    <div className="bg-[#b2c4e2]/[.46] min-h-[500px] flex flex-col">
+      <p className="text-center py-6 font-bold text-xl text-gray-600">بهین کار  سامانه کاریابی انلاین  در جنوب شرق ایران</p>
+      <div className="flex  justify-center items-center gap-4 my-6  rounded-b-3xl ">
+        <input
+          type="text"
+          className="rounded border placeholder:text-[#828080] placeholder:text-sm w-64 border-[#c9cacc] py-2 px-4 focus:outline-none focus:border-2 focus:border-blue-500 focus:shadow-outline-blue "
+          placeholder="عنوان شغلی ..."
+        />
+        <Select
+          className="w-64"
+          classNamePrefix="select"
+          isClearable={true}
+          isRtl={true}
+          isSearchable={true}
+          name="color"
+          placeholder="استان..."
+          options={[
+            { value: "kerman", label: "کرمان" },
+            { value: "rafsenjan", label: "رفسنجان" },
+            { value: "vanilla", label: "مس سرچشمه" },
+          ]}
+        />
+        <Select
+          className="w-64 "
+          classNamePrefix="select"
+          isClearable={true}
+          isRtl={true}
+          isSearchable={true}
+          name="color"
+          placeholder="دسته بندی..."
+          options={[
+            { value: "kerman", label: "مهندسی" },
+            { value: "rafsenjan", label: "راننده" },
+            { value: "vanilla", label: "کارگری " },
+          ]}
+        />
+        <div className="px-4 bg-bgBtn py-1 transition duration-150 ease-in rounded-md cursor-pointer active:scale-95 hover:scale-110">
+          <RiSearch2Line
+            size={25}
+            className="text-gray-700 "
+            color=""
+          />
+        </div>
+      </div>
+      <p className="text-center py-6 font-bold text-sm text-gray-700">مشاهده همه فرصت های شغلی</p>
+
     </div>
   );
 }
