4de9f44e2c71a4862b7f0480387f3bd8269068e4
miillaadddddd <milad7212@gmail.com>
2023-07-25 19:04:36 +0330

    add header

--- app/components/landingPage/TopMenu.jsx
+++ app/components/landingPage/TopMenu.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 function TopMenu() {
   return (
-    <div>TopMenu</div>
-  )
+    <div className="flex p-6 px-8 justify-between">
+      <div className="flex items-center gap-6">
+        <Link href="/#">خانه</Link>
+        <Link href="/#">فرصت های شغلی</Link>
+        <Link href="/#">آکادمی</Link>
+        <Link href="/#">رزومه ساز</Link>
+        <Link href="/#">جستجو مشاغل</Link>
+      </div>
+      <div className="flex">
+        <div className=""></div>
+        <div className="font-black text-colorTitle text-xl">بهین کار</div>
+      </div>
+    </div>
+  );
 }
 
-export default TopMenu
+export default TopMenu;
