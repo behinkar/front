f5145c2dfa00913270de79d6e6efbed513dcb596
miillaadddddd <milad7212@gmail.com>
2023-07-26 09:47:43 +0330

    change ui login

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -10,30 +10,37 @@ function LoginPage() {
   }
   return (
     <div className="">
-      <div className="min-h-screen bg-gray-300  flex justify-center items-center ">
-        <div className="">
-          <p className="font-bold text-sm mb-1 text-gray-500">ورود کارفرما</p>
-          <div className=" rounded-lg shadow-lg bg-gray-50 ">
-            <div className="p-6 pb-0 ">
-              <InputApp label="ایمیل یا شماره موبایل" />
-              <InputApp label="رمز عبور" />
-              <p className="text-xs text-gray-500 mb-4">فراموشی رمز عبور</p>
-              <div className="flex justify-center mb-4">
-                <button className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-8 rounded focus:outline-none focus:shadow-outline transition duration-300">
-                  ورود
-                </button>
+      <div className="grid grid-cols-[2fr_3fr] bg-gray-300">
+        <div
+          className=" bg-contain bg-no-repeat bg-right"
+          style={{ backgroundImage: `url('/images/bgLogin.png')` }}
+        ></div>
+
+        <div className="min-h-screen bg-gray-300  flex justify-center items-center ">
+          <div className="">
+            <p className="font-bold text-sm mb-1 text-gray-500">ورود کارفرما</p>
+            <div className=" rounded-lg shadow-lg bg-gray-50 ">
+              <div className="p-6 pb-0 ">
+                <InputApp label="ایمیل یا شماره موبایل" />
+                <InputApp label="رمز عبور" />
+                <p className="text-xs text-gray-500 mb-4">فراموشی رمز عبور</p>
+                <div className="flex justify-center mb-4">
+                  <button className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-8 rounded focus:outline-none focus:shadow-outline transition duration-300">
+                    ورود
+                  </button>
+                </div>
               </div>
-            </div>
-            <p className="text-xs pr-3 pb-3 text-gray-500 ">
-              <span className="">حساب کارفرمایی ندارید؟</span>
-              <span
-                onClick={handleClick}
-                className=" cursor-pointer hover:font-bold mr-1"
-              >
-                ثبت نام کارفرما
-              </span>
-            </p>
-          </div>{" "}
+              <p className="text-xs pr-3 pb-3 text-gray-500 ">
+                <span className="">حساب کارفرمایی ندارید؟</span>
+                <span
+                  onClick={handleClick}
+                  className=" cursor-pointer hover:font-bold mr-1"
+                >
+                  ثبت نام کارفرما
+                </span>
+              </p>
+            </div>{" "}
+          </div>
         </div>
       </div>
     </div>
--- app/register/page.jsx
+++ app/register/page.jsx
@@ -14,27 +14,35 @@ function RegisterPage() {
         <title>ثبت نام کارفرما</title>
       </Head>
       <div className="grid grid-cols-[2fr_3fr] bg-gray-300">
-        <div className=" bg-contain bg-no-repeat bg-right" style={{ backgroundImage: `url('/images/bgLogin.png')` }}></div>
-        <div className=" min-h-screen">
-          <div className="flex justify-between items-center p-4 text-gray-600 text-sm">
-            <span className="">Logo</span>
-            <span className="">
-              <span  className="">
-                حساب دارید؟
+        <div
+          className=" bg-contain bg-no-repeat bg-right"
+          style={{ backgroundImage: `url('/images/bgLogin.png')` }}
+        ></div>
+        <div className=" min-h-screen flex flex-col relative">
+          <div className="absolute left-0 right-0">
+            <div className="flex  justify-between items-center p-4 text-gray-600 text-sm">
+              <div className="font-black text-colorTitle text-xl">بهین کار</div>
+              <span className="">
+                <span className="">کارفرما هستید؟ </span>
+                <span
+                  onClick={() => handleNavigationClick("/login")}
+                  className=" ml-2 mr-1 hover:font-bold cursor-pointer text-colorTitle"
+                >
+                  ثبت نام
+                </span>
               </span>
-              <span onClick={() => handleNavigationClick("/login")} className=" mr-2 hover:font-bold cursor-pointer">ورود</span>
-            </span>
+            </div>
           </div>
-          <div className=" flex justify-center items-center ">
+          <div className=" flex justify-center items-center  grow">
             <div className="">
               <p className="font-bold text-sm mb-1 text-gray-500">
-                ثبت نام کارفرما
+                ثبت نام کارجو
               </p>
-              <div className=" rounded-lg shadow-lg bg-gray-50 ">
-                <div className="p-6 pb-0 ">
-                  <InputApp label="نام و نام خانوادگی" />
+              <div className=" rounded-lg shadow-lg bg-gray-50  ">
+                <div className="p-6 pb-0  min-w-[300px]">
+                  {/* <InputApp label="نام و نام خانوادگی" /> */}
                   <InputApp label="ایمیل" />
-                  <InputApp label="موبایل" />
+                  {/* <InputApp label="موبایل" /> */}
                   <InputApp label="رمز" />
                   <InputApp label="تکرار رمز" />
                   <div className="flex justify-center pb-5 ">
