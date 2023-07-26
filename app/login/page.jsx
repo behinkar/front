c0afcaceba1acaf553cb1ddef54cdf9f0bb423ba
miillaadddddd <milad7212@gmail.com>
2023-07-26 15:50:08 +0330

    fixed login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,12 +1,14 @@
 "use client";
 import React from "react";
 import InputApp from "../components/Input/InputApp";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 function LoginPage() {
   const router = useRouter();
-  function handleClick() {
-    router.push("/register");
+  const url = useSearchParams();
+  const whichUser = url.get("w");
+  function handleRouteClick(url) {
+    router.push(url);
   }
   return (
     <div className="">
@@ -16,30 +18,52 @@ function LoginPage() {
           style={{ backgroundImage: `url('/images/bgLogin.png')` }}
         ></div>
 
-        <div className="min-h-screen bg-gray-300  flex justify-center items-center ">
-          <div className="">
-            <p className="font-bold text-sm mb-1 text-gray-500">ورود کارفرما</p>
-            <div className=" rounded-lg shadow-lg bg-gray-50 ">
-              <div className="p-6 pb-0 ">
-                <InputApp label="ایمیل یا شماره موبایل" />
-                <InputApp label="رمز عبور" />
-                <p className="text-xs text-gray-500 mb-4">فراموشی رمز عبور</p>
-                <div className="flex justify-center mb-4">
-                  <button className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-8 rounded focus:outline-none focus:shadow-outline transition duration-300">
-                    ورود
-                  </button>
-                </div>
-              </div>
-              <p className="text-xs pr-3 pb-3 text-gray-500 ">
-                <span className="">حساب کارفرمایی ندارید؟</span>
-                <span
-                  onClick={handleClick}
-                  className=" cursor-pointer hover:font-bold mr-1"
-                >
-                  ثبت نام کارفرما
+        <div className="min-h-screen relative bg-gray-300  flex flex-col ">
+        <div className="absolute left-0 right-0">
+            <div className="flex  justify-between items-center p-4 text-gray-600 text-sm">
+              <div className="font-black text-colorTitle text-xl">بهین کار</div>
+              {whichUser!= "employer" && (
+                <span className="">
+                  <span className="">کارفرما هستید؟ </span>
+                  <span
+                    onClick={() =>
+                      handleNavigationClick("/register?w=employer")
+                    }
+                    className=" ml-2 mr-1 hover:font-bold cursor-pointer text-colorTitle"
+                  >
+                     ورود
+                  </span>
                 </span>
+              )}
+            </div>
+          </div>
+          <div className="grow flex justify-center items-center">
+            <div className="">
+              <p className="font-bold text-sm mb-1 text-gray-500">
+                {whichUser == "employer" ? " ورود کارفرما" : " ورود کارجو"}
               </p>
-            </div>{" "}
+              <div className=" rounded-lg shadow-lg bg-gray-50 ">
+                <div className="p-6 pb-0 ">
+                  <InputApp label="ایمیل یا شماره موبایل" />
+                  <InputApp label="رمز عبور" />
+                  <p className="text-xs text-gray-500 mb-4">فراموشی رمز عبور</p>
+                  <div className="flex justify-center mb-4">
+                    <button className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-8 rounded focus:outline-none focus:shadow-outline transition duration-300">
+                      ورود
+                    </button>
+                  </div>
+                </div>
+                <p className="text-xs pr-3 pb-3 text-gray-500 ">
+                  <span className="">حساب کارفرمایی ندارید؟</span>
+                  <span
+                    onClick={()=>handleRouteClick('/register?w=employer')}
+                    className=" cursor-pointer hover:font-bold mr-1"
+                  >
+                    ثبت نام کارفرما
+                  </span>
+                </p>
+              </div>{" "}
+            </div>
           </div>
         </div>
       </div>
--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,10 +1,15 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import InputApp from "../components/Input/InputApp";
 import Head from "next/head";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import Link from "next/link";
 function RegisterPage() {
+  // const [whichUser, setWhichUser] = useState('jobSeeker')
   const router = useRouter();
+  const url = useSearchParams();
+  const whichUser = url.get("w");
+
   function handleNavigationClick(url) {
     router.push(url);
   }
@@ -22,10 +27,21 @@ function RegisterPage() {
           <div className="absolute left-0 right-0">
             <div className="flex  justify-between items-center p-4 text-gray-600 text-sm">
               <div className="font-black text-colorTitle text-xl">بهین کار</div>
+
               <span className="">
-                <span className="">کارفرما هستید؟ </span>
+                {whichUser != "employer" ? (
+                  <span className="">کارفرما هستید؟ </span>
+                ) : (
+                  <span className="">کارجو هستید؟ </span>
+                )}
                 <span
-                  onClick={() => handleNavigationClick("/login")}
+                  onClick={() => {
+                    if (whichUser != "employer") {
+                      handleNavigationClick("/register?w=employer");
+                    } else {
+                      handleNavigationClick("/register");
+                    }
+                  }}
                   className=" ml-2 mr-1 hover:font-bold cursor-pointer text-colorTitle"
                 >
                   ثبت نام
@@ -36,20 +52,31 @@ function RegisterPage() {
           <div className=" flex justify-center items-center  grow">
             <div className="">
               <p className="font-bold text-sm mb-1 text-gray-500">
-                ثبت نام کارجو
+                {whichUser == "employer" ? "ثبت نام کارفرما" : "ثبت نام کارجو"}
               </p>
               <div className=" rounded-lg shadow-lg bg-gray-50  ">
                 <div className="p-6 pb-0  min-w-[300px]">
-                  {/* <InputApp label="نام و نام خانوادگی" /> */}
                   <InputApp label="ایمیل" />
-                  {/* <InputApp label="موبایل" /> */}
                   <InputApp label="رمز" />
                   <InputApp label="تکرار رمز" />
+
                   <div className="flex justify-center pb-5 ">
                     <button className="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-8 rounded focus:outline-none focus:shadow-outline transition duration-300">
                       ثبت نام
                     </button>
                   </div>
+
+                  <p className="my-4 p-2 text-sm  text-gray-600">
+                    حساب کاربری دارید؟
+                    <Link
+                      href={
+                        whichUser == "employer" ? "/login?w=employer" : "/login"
+                      }
+                      className="mx-2 hover:text-colorTitle hover:font-bold"
+                    >
+                      ورود
+                    </Link>
+                  </p>
                 </div>
               </div>{" "}
             </div>
