618a3af8df6a27492820a9ae308f0720efa48a24
miillaadddddd <milad7212@gmail.com>
2023-07-29 14:34:04 +0330

    finish search page

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import SearchPart from "../components/searchPage/SearchPart";
-import { TopMenu } from "../components/landingPage";
+import { Footer, TopMenu } from "../components/landingPage";
+import ResultPart from "../components/searchPage/ResultPart";
 
 function page() {
   return (
-    <div className="">
+    <div className="px-4">
       <TopMenu />
       <SearchPart />
+      <ResultPart/>
+      <Footer />
     </div>
   );
 }
