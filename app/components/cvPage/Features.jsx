ac8cda396057e0a43c0aea3fbc3a7ea3f5921b6b
Milad Hasani <milad7212@gmail.com>
2023-08-20 21:16:49 +0330

    finish ui

--- app/components/cvPage/Features.jsx
+++ app/components/cvPage/Features.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { FiClock, FiRefreshCw } from "react-icons/fi";
+import { FaShareAlt, FaMedal } from "react-icons/fa";
+import { BiHourglass } from "react-icons/bi";
+import { BsFillPeopleFill } from "react-icons/bs";
 
 function Features() {
   return (
@@ -8,16 +12,43 @@ function Features() {
           ویژگی های رزومه ساز بهین کار
         </p>
       </div>
-      <div className="grid grid-cols-2 my-20 gap-20 text-[#2C4875]  font-bold max-w-4xl mx-auto">
+      <div className="grid grid-cols-2 my-20 gap-20 text-[#2C4875]  font-bold max-w-3xl mx-auto">
         <div className=" flex flex-col gap-16">
-          <span className="mr-auto w-40">امکان اشتراک گذاری</span>
-          <p className="">طراحی حرفه ای و استاندارد</p>
-          <p className="mr-auto w-40">ساخت سریع</p>
+          <div className="mr-auto w-52 flex items-center ">
+            <FaShareAlt className="ml-2" size={35} />
+
+            <span className=""> امکان اشتراک گذاری</span>
+          </div>
+
+          <p className="flex items-center">
+            <span className="ml-2">
+              <FaMedal size={35} />
+            </span>
+            <span>طراحی حرفه ای و استاندارد</span>
+          </p>
+          <div className="mr-auto w-52 flex items-center">
+            <BiHourglass className="ml-2" size={35} />
+            ساخت سریع
+          </div>
         </div>
         <div className="flex flex-col gap-16">
-          <p className="">صرفه جویی در هزینه و زمان</p>
-          <p className="mr-auto ">به روز رسانی همیشگی</p>
-          <p className="">در دید کارفرمایان</p>
+          <p className="flex items-center">
+            <span className="ml-2">
+              <FiClock size={35} />
+            </span>
+            <span>صرفه جویی در هزینه و زمان</span>
+          </p>
+          <p className="mr-auto flex items-center ">
+            <FiRefreshCw className="ml-2" size={35} />
+            <span>به روز رسانی همیشگی</span>
+          </p>
+          <div className="flex items-center">
+            <span className="ml-2">
+              <BsFillPeopleFill size={35} />
+            </span>
+
+            <span>در دید کارفرمایان</span>
+          </div>
         </div>
       </div>
     </>
