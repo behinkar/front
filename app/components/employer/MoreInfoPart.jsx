360749c320d83f3684c7cb97cd721910034fe664
Milad Hasani <milad7212@gmail.com>
2023-08-31 11:39:59 +0330

    Add icons

--- app/components/employer/MoreInfoPart.jsx
+++ app/components/employer/MoreInfoPart.jsx
@@ -2,24 +2,26 @@ import Image from "next/image";
 import React from "react";
 import chairImage from "@/public/images/chair.png";
 
+import { AiFillCaretLeft } from "react-icons/ai";
+
+const data = [
+  " انتشار در فهرست فرصت های شغلی بهین کار",
+  " ارسال ایمیل به کارجویان مرتبط",
+  " درج در شبکه های اجتماعی بهین کار",
+];
+
 function MoreInfoPart() {
   return (
     <>
       <div className="grid grid-cols-2 gap-6 my-20 max-w-6xl mx-auto ">
         <div className="">
-          <p className="text-colorTitle font-bold text-2xl">
+          <p className="text-colorTitle font-bold text-2xl mb-16">
             آگهی های شغلی در کجا منتشر میشوند؟
           </p>
           <div className="h-full flex flex-col gap-16">
-            <p className="text-[#567EBF] font-bold pr-8 text-xl mt-24">
-              انتشار در فهرست فرصت های شغلی بهین کار
-            </p>
-            <p className="text-[#567EBF] font-bold pr-8 mt-8 text-xl">
-              ارسال ایمیل به کارجویان مرتبط
-            </p>
-            <p className="text-[#567EBF] font-bold pr-8 mt-8 text-xl">
-              درج در شبکه های اجتماعی بهین کار
-            </p>
+            {data.map((item, index) => (
+              <Title data={item} key={index} />
+            ))}
           </div>
         </div>
         <div className="flex justify-center items-center">
@@ -31,3 +33,14 @@ function MoreInfoPart() {
 }
 
 export default MoreInfoPart;
+
+function Title({ data }) {
+  return (
+    <>
+      <div className="flex gap-4 pr-8 items-center mt-8 ">
+        <AiFillCaretLeft size={25} color="#FFA702" />
+        <p className="text-[#567EBF] font-bold  text-xl">{data}</p>
+      </div>
+    </>
+  );
+}
