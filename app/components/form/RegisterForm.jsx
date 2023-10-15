303fe8084e9f4720ad41964e9122c4d7160e6f80
Milad Hasani <milad7212@gmail.com>
2023-10-15 19:36:03 +0330

    fixed button

--- app/components/form/RegisterForm.jsx
+++ app/components/form/RegisterForm.jsx
@@ -1,9 +1,9 @@
-"use client";
 import { Button } from "@material-tailwind/react";
-import classNames from "classnames";
+
 import { useForm } from "react-hook-form";
 
 import axios from "axios";
+import AppBtn from "../share/AppBtn";
 
 function RegisterForm() {
   const {
@@ -11,59 +11,47 @@ function RegisterForm() {
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const callApi = async (value) => {
-    if (value.phone_number === "") {
-      return;
-    }
-
-    const { data } = await axios.post(
-      "https://api.behinkar.ir/accounts/reggister/",
-
-      value,
-      {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json;charset=utf-8",
-      }
-    );
+  const onSubmit = (value) => {
+    console.log(value);
+    // if (value.phone_number === "") {
+    //   return;
+    // }
+    // const { data } = await axios.post(
+    //   "https://api.behinkar.ir/accounts/reggister/",
+    //   value,
+    //   {
+    //     "Access-Control-Allow-Origin": "*",
+    //     "Content-Type": "application/json;charset=utf-8",
+    //   }
+    // );
   };
   return (
-    <form onSubmit={handleSubmit(callApi)}>
-      <label className="label">شماره همراه</label>
-      <input
-        className="input-class"
-        type="tel"
-        maxLength="11"
-        placeholder="09139939426"
-      />
-      {Boolean(errors?.phone_number?.message) && (
-        <span className="error-label">{errors.phone_number.message} </span>
-      )}
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label className="label">شماره همراه</label>
+        <input
+          className="input-class"
+          type="tel"
+          maxLength="11"
+          placeholder="09139939426"
+          {...register("phone_number")}
+        />
+        {/* {Boolean(errors?.phone_number?.message) && (
+          <span className="error-label">{errors.phone_number.message} </span>
+        )} */}
 
-      <label className="label mt-4">رمز عبور </label>
-      <input
-        className={`input-class ${classNames({
-          "focus:border-red-400    border-red-400 border-2":
-            errors?.phoneNumber?.message,
-        })}`}
-        type="tel"
-        {...register("password1")}
-      />
-      <label className="label mt-4">تکرار رمز عبور </label>
-      <input
-        className={`input-class ${classNames({
-          "focus:border-red-400    border-red-400 border-2":
-            errors?.phoneNumber?.message,
-        })}`}
-        type="tel"
-        {...register("password2")}
-      />
+        <label className="label mt-4">رمز عبور </label>
+        <input className="input-class" type="tel" {...register("password1")} />
+        <label className="label mt-4">تکرار رمز عبور </label>
+        <input className="input-class" type="tel" {...register("password2")} />
 
-      <div className="flex justify-center pb-5  mt-6">
-        <Button type="submit" className="bg-[#567EBF]" size="lg">
-          دریافت کد فعال سازی
-        </Button>
-      </div>
-    </form>
+        <div className="flex justify-center pb-5  mt-6">
+          <AppBtn type="submit" className="bg-[#567EBF]">
+            دریافت کد فعال سازی
+          </AppBtn>
+        </div>
+      </form>
+    </>
   );
 }
 
--- app/components/share/AppBtn.jsx
+++ app/components/share/AppBtn.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { twMerge } from "tailwind-merge";
-function AppBtn({ label = "ساخت رزومه", className }) {
+function AppBtn({ label = "ساخت رزومه", className, type }) {
   return (
     <button
+      type={type}
       className={twMerge(
         "inline-block  bg-bgBtn text-sm hover:scale-105 active:scale-95 whitespace-nowrap text-gray-800 font-bold py-3 px-8 rounded-full focus:outline-none focus:shadow-outline transition duration-300",
         className
